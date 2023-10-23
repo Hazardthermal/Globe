7e8b6d8a6120d1d9894a069779081747ec99ed57
Hazardthermal <140638535+Hazardthermal@users.noreply.github.com>
2023-10-23 10:23:29 -0500

    Add files via upload

---
+++ assets/data/connections.js
@@ -0,0 +1,5 @@
+// data.connections = {"United States":["Australia","Canada","Czech Republic","India","Liechtenstein","Nepal","Panama","Singapore"]}
+
+data.connections = {"United States":["Singapore","Canada","Australia","Austria","Bahamas","Brazil","Cayman Islands","Channel Islands of Jersey and Guernsey","China","Cook Islands","Costa Rica","Czech Republic","France","Germany","India","Isle of Man","Israel","Italy","Liechtenstein","Malta","Mexico","Nepal","Netherlands","Nevis & St Kitts","New Zealand","Oman","Panama","Portugal","Puerto Rico","Spain","Switzerland","UAE","United Kingdom","Uruguay","USVI"]}
+// data.connections = {"United States":["Canada"]}
+data.connections = getCountries(data.connections, data.countries)
